fab67325de73d02ee388d2723d17aeeff157149d
AyeshAbuLehyeh <165114905+AyeshAbuLehyeh@users.noreply.github.com>
2025-11-16 01:38:25 +0000

    Deploying to gh-pages from @ AyeshAbuLehyeh/AyeshAbuLehyeh.github.io@528ab9107c42565579c4e8bfaafad7d2952e51d5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -85,11 +85,6 @@ ninja.data = [{
           description: "Reducing semiconductor test time and cost at GlobalFoundries using machine learning.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
