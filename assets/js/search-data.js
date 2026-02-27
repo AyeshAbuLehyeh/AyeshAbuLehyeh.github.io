0786811305bde26c6165122b94dc1a47fb5b3298
AyeshAbuLehyeh <ayesh.abulehyeh@uvm.edu>
2026-02-27 04:44:32 +0000

    Deploying to gh-pages from @ AyeshAbuLehyeh/AyeshAbuLehyeh.github.io@23c6f16a063cde91d1de32f5a56ac9b782b42eef 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -83,7 +83,7 @@ ninja.data = [{
               window.location.href = "/projects/1_project/";
             },},{id: "projects-spatially-adaptive-conformal-graph-transformer-for-indoor-localization-in-wi-fi-driven-networks",
           title: 'Spatially-Adaptive Conformal Graph Transformer for Indoor Localization in Wi-Fi Driven Networks',
-          description: "A GNN-based regression framework for indoor localization with rigorous uncertainty guarantees.",
+          description: "A GNN-based regression framework for indoor localization with rigorous uncertainty guarantees. Accepted to IEEE ICC 2026.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-ml-driven-rf-test-compaction",
